cf7be432a3e396e2280ed1d88e91d642f6cc0a32
Lee-Roy King <leeroywking@gmail.com>
2021-12-16 12:05:22 -0800

    fixed carriage returns

--- affirmations.py
+++ affirmations.py
@@ -7,12 +7,16 @@ with open("./static/affirmations.txt") as file:
         affirmations.append(line.strip())
 
 
-def get_affirmation():
-    affirm = affirmations[random.randint(0,len(affirmations))]
+def get_affirmation(idx = None):
+    if idx:
+        affirm = affirmations[idx]
+    else:
+        affirm = affirmations[random.randint(0,len(affirmations))]
     [words, author] = affirm.split(" - ")
+    words = words.replace("\\n", "\n")
     output = f"{words}\n   - {author}"
     # print(output)
     return output
 
 if __name__ == "__main__":
-    get_affirmation()
+    print(get_affirmation(-1))
